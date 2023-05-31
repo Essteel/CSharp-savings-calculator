6ef6a5095a5a502213ee7478b5735dd6882b6cc4
Essteel <siannon.steel@gmail.com>
2023-05-31 13:52:16 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Please enter the initial deposit:");
+var depositInput = Console.ReadLine();
+var deposit = double.Parse(depositInput);
+
+Console.WriteLine("Please enter the monthly interest rate:");
+var interestRateInput = Console.ReadLine();
+var interestRate = double.Parse(interestRateInput);
+
+Console.WriteLine("How many months will you save for?");
+var monthsSavedInput = Console.ReadLine();
+var monthsSaved = int.Parse(monthsSavedInput);
+
+var savings = deposit * Math.Pow(1 + (interestRate/100), monthsSaved);
+Console.WriteLine($"After {monthsSaved} months you will have {savings}");
